39109f7f6a2dcb0a93f2852cc06e11c4c12d1c35
MasanoriFukuda2312 <uni202312_08@telework.to>
2024-02-18 00:16:18 +0900

    Cardクラス作成

---
+++ blackjack/Card.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blackjack
+{
+    enum CardSuit
+    {
+        Club,
+        Spade,
+        Heart,
+        Diamond
+    internal class Card
+    {
+
+        public CardSuit Suit;
+        public int Number;
+        public int Id;
+        public int Point;
+        public int SumPoint;
+        public string Address;
+        public Card(CardSuit suit, int number, int id, int point, string address)
+        {
+            Suit = suit;
+            Number = number;
+            Id = id;
+            Point = point;
+            Address = address;
+        }
+        public int PointCount(int number)
+        {
+            if (Number == 1)
+            {
+                Point = 11;
+            }
+            if (Number >= 10)
+            {
+                Point = 10;
+            }
+            return Point;
+        }
+    }
+}
+
